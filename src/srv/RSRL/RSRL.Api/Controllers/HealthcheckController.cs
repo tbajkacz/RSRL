2eae24e536bdddf6606ec37bb0073559409b97d3
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-10 12:42:05 +0200

    Initialized web and server projects

---
+++ src/srv/RSRL/RSRL.Api/Controllers/HealthcheckController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSRL.Api.Utility;
+using System.Net;
+
+namespace RSRL.Api.Controllers
+{
+    [ApiController]
+    [DefaultRoute]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        public void Heartbeat()
+        {
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+        }
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Utility/DefaultRouteAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RSRL.Api.Utility
+{
+    public class DefaultRouteAttribute : RouteAttribute
+    {
+        public DefaultRouteAttribute()
+            : base("api/[controller]/[action]")
+        {
+        }
+    }
+}
