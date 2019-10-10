1667840650eb67b16335fd222bc224377737dfb0
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-10 21:05:34 +0200

    Added lock control endpoints

---
+++ src/srv/RSRL/RSRL.Api/Controllers/ControlController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSRL.Api.Locks.Params;
+using RSRL.Api.Locks.Services;
+using RSRL.Api.Utility;
+using System.Threading.Tasks;
+
+namespace RSRL.Api.Controllers
+{
+    [ApiController]
+    [DefaultRoute]
+    public class ControlController : ControllerBase
+    {
+        private readonly IRemoteLock remoteLock;
+
+        public ControlController(IRemoteLock remoteLock)
+        {
+            this.remoteLock = remoteLock;
+        }
+
+        public async Task ToggleBlock(ToggleBlockParams param)
+        {
+            await remoteLock.ToggleBlockAsync(param.TargetState);
+        }
+
+        public async Task Unlock()
+        {
+            await remoteLock.UnlockAsync();
+        }
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Locks/Params/ToggleBlockParams.cs
@@ -0,0 +1,7 @@
+﻿namespace RSRL.Api.Locks.Params
+{
+    public class ToggleBlockParams
+    {
+        public bool TargetState { get; set; }
+    }
+}
