fd5b171883ee3ee05562354980e3eb25f05b0e9f
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-10 21:04:53 +0200

    Added IRemoteLock interface and a temporary implementation

---
+++ src/srv/RSRL/RSRL.Api/Locks/Services/ArduinoRemoteLock.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace RSRL.Api.Locks.Services
+{
+    public class ArduinoRemoteLock : IRemoteLock
+    {
+        public async Task ToggleBlockAsync(bool targetState)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task UnlockAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Locks/Services/IRemoteLock.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace RSRL.Api.Locks.Services
+{
+    public interface IRemoteLock
+    {
+        Task UnlockAsync();
+
+        Task ToggleBlockAsync(bool targetState);
+    }
+}
