98de7b03cf6e6fa74637877b98ba832c2983d2bc
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-16 11:26:48 +0200

    Added missing NHibernate id convention

---
+++ src/srv/RSRL/RSRL.Api/Db/Conventions/IncrementIdConvention.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.Instances;
+using System;
+
+namespace RSRL.Api.Db.Conventions
+{
+    public class IncrementIdConvention : IIdConvention
+    {
+        public void Apply(IIdentityInstance instance)
+        {
+            if (instance.Type.Name.Contains("string", StringComparison.InvariantCultureIgnoreCase))
+            {
+                instance.GeneratedBy.Increment();
+            }
+        }
+    }
+}
