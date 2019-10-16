fb8a645b5d5aa0b16c7bcb1b2345047d57046223
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-16 10:51:21 +0200

    Added extension method which adds nhibernate session factory to the DI container

---
+++ src/srv/RSRL/RSRL.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Microsoft.Extensions.DependencyInjection;
+using NHibernate.Tool.hbm2ddl;
+using RSRL.Api.Db.Conventions;
+using System.Data;
+
+namespace RSRL.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddNHibernateSessionFactory(this IServiceCollection services, string connectionString)
+        {
+            var fact = Fluently.Configure()
+                .Database(SQLiteConfiguration.Standard.UsingFile(connectionString).IsolationLevel(IsolationLevel.ReadCommitted))
+                .Mappings(x => x.FluentMappings.AddFromAssemblyOf<Program>().Conventions.Add<IncrementIdConvention>())
+                .ExposeConfiguration(x =>
+                    new SchemaUpdate(x).Execute(false, true))
+                .BuildSessionFactory();
+            return services.AddScoped<NHibernate.ISession>(p => fact.OpenSession());
+        }
+    }
+}
