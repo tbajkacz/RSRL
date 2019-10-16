9d6f3e8a33310bca145194be0ca8c606e22188ca
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-16 11:22:58 +0200

    Added base model class for entities

---
+++ src/srv/RSRL/RSRL.Api/Db/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace RSRL.Api.Db.Models
+{
+    public class Entity<TId>
+    {
+        public virtual TId Id { get; set; }
+    }
+
+    public class EntityMap<TId> : ClassMap<Entity<TId>>
+    {
+        public EntityMap()
+        {
+            Id(x => x.Id);
+        }
+    }
+}
