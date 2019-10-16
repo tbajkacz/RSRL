bcf0ca1360d1b0e538f0b6329e2c872e1aa3dad7
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-16 11:26:14 +0200

    Added IUnitOfWork interface and an nhibernate implementation

---
+++ src/srv/RSRL/RSRL.Api/Db/Services/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RSRL.Api.Db.Services
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task CommitAsync();
+
+        Task RollbackAsync();
+    }
+}
