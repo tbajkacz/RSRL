b3824b8c825cb5b24e576f86b8a8cf8f6f45b204
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-19 13:33:10 +0200

    Added missing HashOptions model

---
+++ src/srv/RSRL/RSRL.Api/Options/HashOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace RSRL.Api.Options
+{
+    public class HashOptions
+    {
+        public int? SaltSize { get; set; }
+
+        public int? Iterations { get; set; }
+
+        public int? KeySize { get; set; }
+
+        public string HashAlgorithmName { get; set; }
+    }
+}
