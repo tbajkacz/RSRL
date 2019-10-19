95330e03555a8e766e64c212cd704271286a248f
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-19 13:31:17 +0200

    Added extension method for signing in

---
+++ src/srv/RSRL/RSRL.Api/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using RSRL.Api.Users.Models;
+using System.Threading.Tasks;
+
+namespace RSRL.Api.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static async Task SignInAsync(this HttpContext httpContext, UserAccount user, bool rememberMe)
+            => await httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                             user.GenerateClaims(),
+                                             new AuthenticationProperties
+                                             {
+                                                 AllowRefresh = true,
+                                                 IsPersistent = rememberMe,
+                                             });
+    }
+}
