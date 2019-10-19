db2d211bc6d2c894502a5b88157ce51dbb53c589
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-19 13:30:59 +0200

    Added extension methods which configure authentication and authorization in the service collection

--- src/srv/RSRL/RSRL.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/srv/RSRL/RSRL.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,10 +1,20 @@
 ﻿using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.DependencyInjection;
 using NHibernate.Tool.hbm2ddl;
+using RSRL.Api.Auth.Constants;
 using RSRL.Api.Db.Conventions;
 using RSRL.Api.Db.Services;
+using RSRL.Api.Exceptions;
+using RSRL.Api.Users.Services;
+using System;
 using System.Data;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace RSRL.Api.Extensions
 {
@@ -21,5 +31,52 @@ namespace RSRL.Api.Extensions
             return services.AddScoped<NHibernate.ISession>(p => fact.OpenSession())
                 .AddScoped<IUnitOfWork, NHibernateUnitOfWork>();
         }
+
+        public static IServiceCollection AddEntitySessions(this IServiceCollection services)
+            => services.AddTransient<IUserSession, NHibernateUserSession>();
+
+        public static AuthenticationBuilder AddCookieAuthentication(this IServiceCollection services)
+            => services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(cfg =>
+                {
+                    cfg.Events = new CookieAuthenticationEvents
+                    {
+                        OnValidatePrincipal = async context =>
+                        {
+                            var userSession = context.HttpContext.RequestServices.GetRequiredService<IUserSession>();
+                            try
+                            {
+                                var userId = context.Principal.GetId();
+                                var user = await userSession.GetByIdAsync(userId);
+                                var claimRoles = context.Principal.Claims.Where(c => c.Type == ClaimTypes.Role)
+                                    .Select(c => c.Value);
+                                if (!(claimRoles.Except(user.Roles).Count() == 0 && user.Roles.Except(claimRoles).Count() == 0))
+                                {
+                                    context.RejectPrincipal();
+                                }
+                            }
+                            catch (Exception e) when (e is FormatException || e is EntityNotFoundException)
+                            {
+                                context.RejectPrincipal();
+                            }
+                        },
+                        OnRedirectToAccessDenied = context =>
+                        {
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                            return Task.CompletedTask;
+                        },
+                        OnRedirectToLogin = context =>
+                        {
+                            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            return Task.CompletedTask;
+                        },
+                    };
+                });
+
+        public static IServiceCollection AddAuthorizationWithPolicies(this IServiceCollection services)
+            => services.AddAuthorization(cfg =>
+            {
+                cfg.AddPolicy(Policies.Admin, p => p.RequireRole(Roles.Admin));
+            });
     }
 }
