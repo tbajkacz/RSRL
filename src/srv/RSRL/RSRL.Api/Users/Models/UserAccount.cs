6a898237ca13f65da23c5e8f74768611411578fb
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-19 13:28:05 +0200

    Added UserAccount model

---
+++ src/srv/RSRL/RSRL.Api/Users/Models/UserAccount.cs
@@ -0,0 +1,29 @@
+﻿using RSRL.Api.Db.Models;
+using System.Collections.Generic;
+
+namespace RSRL.Api.Users.Models
+{
+    public class UserAccount : Entity<int>
+    {
+        public virtual string Login { get; set; }
+
+        public virtual string PasswordHash { get; set; }
+
+        public virtual IList<string> Roles { get; set; } = new List<string>();
+    }
+
+    public class UserAccountMap : EntityMap<UserAccount, int>
+    {
+        public UserAccountMap()
+        {
+            Map(x => x.Login)
+                .Not.Nullable()
+                .Unique();
+            Map(x => x.PasswordHash)
+                .Not.Nullable();
+            HasMany(x => x.Roles)
+                .Element("Roles")
+                .Cascade.SaveUpdate();
+        }
+    }
+}
