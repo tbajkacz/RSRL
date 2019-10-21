b1d93e577918df1f47ccdb1250872262d45525fc
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-21 11:51:46 +0200

    Added dto for user model

---
+++ src/srv/RSRL/RSRL.Api/Users/Dto/UserAccountDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RSRL.Api.Users.Dto
+{
+    public class UserAccountDto
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public IList<string> Roles { get; set; }
+    }
+}
