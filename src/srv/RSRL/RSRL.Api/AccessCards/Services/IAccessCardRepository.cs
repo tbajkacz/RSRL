943058986f3d99cb66d885de8750674dc931e192
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-23 10:57:51 +0200

    Added access card repository and a basic implementation

---
+++ src/srv/RSRL/RSRL.Api/AccessCards/Services/IAccessCardRepository.cs
@@ -0,0 +1,10 @@
+﻿using RSRL.Api.AccessCards.Models;
+using RSRL.Api.Db.Services;
+
+namespace RSRL.Api.AccessCards.Services
+{
+    public interface IAccessCardRepository : IRepository<AccessCard, string>
+    {
+
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/AccessCards/Services/NHibernateAccessCardRepository.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using RSRL.Api.AccessCards.Models;
+using RSRL.Api.Db.Services;
+
+namespace RSRL.Api.AccessCards.Services
+{
+    public class NHibernateAccessCardRepository : NHibernateRepositoryBase<AccessCard, string>, IAccessCardRepository
+    {
+        public NHibernateAccessCardRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
