cea378795d019af6934d732b8181d0799afe8661
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-25 20:13:47 +0200

    Added lock repository

---
+++ src/srv/RSRL/RSRL.Api/Locks/Services/IRemoteLockRepository.cs
@@ -0,0 +1,9 @@
+﻿using RSRL.Api.Db.Services;
+using RSRL.Api.Locks.Models;
+
+namespace RSRL.Api.Locks.Services
+{
+    public interface IRemoteLockRepository : IRepository<RemoteLock, int>
+    {
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Locks/Services/NHibernateRemoteLockRepository.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using RSRL.Api.Db.Services;
+using RSRL.Api.Locks.Models;
+
+namespace RSRL.Api.Locks.Services
+{
+    public class NHibernateRemoteLockRepository : NHibernateRepositoryBase<RemoteLock, int>, IRemoteLockRepository
+    {
+        public NHibernateRemoteLockRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
