1ace1481b5900dfa04a6541606f088268bd6d18c
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-29 11:21:16 +0100

    Added action logging to endpoints

--- src/srv/RSRL/RSRL.Api/Controllers/LocksController.cs
+++ src/srv/RSRL/RSRL.Api/Controllers/LocksController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using RSRL.Api.Audit.Models;
+using RSRL.Api.Audit.Services;
 using RSRL.Api.Db.Services;
+using RSRL.Api.Extensions;
 using RSRL.Api.Locks.Dto;
 using RSRL.Api.Locks.Models;
 using RSRL.Api.Locks.Params;
@@ -20,14 +23,16 @@ namespace RSRL.Api.Controllers
         private readonly IRemoteLockRepository lockRepository;
         private readonly IMapper mapper;
         private readonly IUnitOfWork uow;
+        private readonly IActionLogger actionLogger;
 
         public LocksController(ILockHttpService lockHttpService, IRemoteLockRepository lockRepository,
-                               IMapper mapper, IUnitOfWork uow)
+                               IMapper mapper, IUnitOfWork uow, IActionLogger actionLogger)
         {
             this.lockHttpService = lockHttpService;
             this.lockRepository = lockRepository;
             this.mapper = mapper;
             this.uow = uow;
+            this.actionLogger = actionLogger;
         }
 
         [HttpGet]
@@ -49,12 +54,22 @@ namespace RSRL.Api.Controllers
         public async Task Unlock(UnlockParams param)
         {
             await lockHttpService.UnlockAsync(param.LockId);
+            await actionLogger.AddActionLogAsync(
+                $"Lock id: {param.LockId} was unlocked",
+                ActionType.LockRemoteUnlock,
+                HttpContext.User.GetId());
+            await uow.CommitAsync();
         }
 
         [HttpPost]
         public async Task ToggleBlock(ToggleBlockParams param)
         {
             await lockHttpService.ToggleBlockAsync(param.LockId, param.TargetState);
+            await actionLogger.AddActionLogAsync(
+                $"Lock id: {param.LockId} was {(param.TargetState ? "blocked" : "unblocked")}",
+                ActionType.LockRemoteToggleBlock,
+                HttpContext.User.GetId());
+            await uow.CommitAsync();
         }
     }
 }
