ae614baba71f48c4acd1efe7716d2957b942158f
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-29 11:20:40 +0100

    Added models for action logs

---
+++ src/srv/RSRL/RSRL.Api/Audit/Models/ActionLog.cs
@@ -0,0 +1,30 @@
+﻿using RSRL.Api.Db.Models;
+using RSRL.Api.Users.Models;
+using System;
+
+namespace RSRL.Api.Audit.Models
+{
+    public class ActionLog : Entity<int>
+    {
+        public virtual UserAccount Executor { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual string Type { get; set; }
+
+        public virtual DateTime ExecutionDate { get; set; }
+    }
+
+    public class ActionLogMap : EntityMap<ActionLog, int>
+    {
+        public ActionLogMap()
+        {
+            References(x => x.Executor);
+            Map(x => x.Description)
+                .Not.Nullable();
+            Map(x => x.Type)
+                .Not.Nullable();
+            Map(x => x.ExecutionDate);
+        }
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Audit/Models/ActionType.cs
@@ -0,0 +1,9 @@
+﻿namespace RSRL.Api.Audit.Models
+{
+    public enum ActionType
+    {
+        LockRemoteToggleBlock,
+        LockRemoteUnlock,
+        LockRfidOpen,
+    }
+}
