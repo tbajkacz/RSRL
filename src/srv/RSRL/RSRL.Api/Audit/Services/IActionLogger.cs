f8b89083be10fbe6320b749fd03d70896be296b8
Tomasz Bajkacz <menidas98@gmail.com>
2019-10-29 11:21:05 +0100

    Added a service for logging lock actions

---
+++ src/srv/RSRL/RSRL.Api/Audit/Services/IActionLogger.cs
@@ -0,0 +1,17 @@
+﻿using RSRL.Api.Audit.Models;
+using RSRL.Api.Locks.Params;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RSRL.Api.Audit.Services
+{
+    public interface IActionLogger
+    {
+        IEnumerable<ActionLog> GetLogs();
+
+        Task AddActionLogAsync(string description, ActionType actionType, int userId = default);
+
+        Task AddActionLogAsync(string description, string actionType, int userId = default);
+    }
+}
---
+++ src/srv/RSRL/RSRL.Api/Audit/Services/NHibernateActionLogger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NHibernate;
+using RSRL.Api.Audit.Models;
+using RSRL.Api.Users.Services;
+
+namespace RSRL.Api.Audit.Services
+{
+    public class NHibernateActionLogger : IActionLogger
+    {
+        private readonly ISession session;
+        private readonly IUserRepository userRepository;
+
+        public NHibernateActionLogger(ISession session, IUserRepository userRepository)
+        {
+            this.session = session;
+            this.userRepository = userRepository;
+        }
+
+        private async Task SaveAsync(ActionLog log)
+        {
+            await session.SaveAsync(log);
+        }
+
+        public IEnumerable<ActionLog> GetLogs()
+        {
+            return session.Query<ActionLog>();
+        }
+
+        public async Task AddActionLogAsync(string description, ActionType actionType, int userId = default)
+        {
+            await AddActionLogAsync(description, actionType.ToString(), userId);
+        }
+
+        public async Task AddActionLogAsync(string description, string actionType, int userId = default)
+        {
+            await SaveAsync(new ActionLog
+            {
+                Description = description,
+                Executor = await userRepository.GetByIdOrDefaultAsync(userId),
+                Type = actionType,
+                ExecutionDate = DateTime.Now,
+            });
+        }
+    }
+}
