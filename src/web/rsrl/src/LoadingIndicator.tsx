db0b2e4e995c2ec427f5d5deff3e36bf3014104a
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-12 16:18:34 +0100

    Added a prop allowing to display the loading indicator as a modal

--- src/web/rsrl/src/LoadingIndicator.tsx
+++ src/web/rsrl/src/LoadingIndicator.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Modal, ModalBody } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 interface LoadingIndicatorProps {
   children?: JSX.Element;
   promise: Promise<any> | undefined;
+  asModal?: boolean;
 }
 
 export default function LoadingIndicator(props: LoadingIndicatorProps) {
@@ -11,6 +15,23 @@ export default function LoadingIndicator(props: LoadingIndicatorProps) {
   useEffect(() => {
     setIsCompleted(false);
   }, [props.promise]);
+
+  const renderLoadingIndicator = () => {
+    const loadingIndicator = (
+      <div className="d-flex justify-content-center">
+        <FontAwesomeIcon icon={faSpinner} spin={true} color="white" size="2x" />
+      </div>
+    );
+    console.log(props.asModal);
+    if (props.asModal) {
+      return (
+        <Modal className="ui-bg-transparent" isOpen={!isCompleted}>
+          <ModalBody>{loadingIndicator}</ModalBody>
+        </Modal>
+      );
+    }
+    return loadingIndicator;
+  };
 
   if (props.promise) {
     props.promise.then(
@@ -21,7 +42,7 @@ export default function LoadingIndicator(props: LoadingIndicatorProps) {
         setIsCompleted(true);
       }
     );
-    return <div>{isCompleted ? props.children : "Loading..."}</div>;
+    return <div>{isCompleted ? props.children : renderLoadingIndicator()}</div>;
   }
   return <div>{props.children}</div>;
 }
