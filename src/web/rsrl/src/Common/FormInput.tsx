ab1f074894bd46c22a89e4a2f6b2169909f795c5
Tomasz Bajkacz <menidas98@gmail.com>
2019-11-17 23:21:08 +0100

    Added isHidden parameter and a default error message

--- src/web/rsrl/src/Common/FormInput.tsx
+++ src/web/rsrl/src/Common/FormInput.tsx
@@ -5,18 +5,21 @@ import { FormGroup } from "reactstrap";
 export interface FormInputConfig {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   isDisabled: (name: string) => boolean;
+  isHidden?: (name: string) => boolean;
   errors: Partial<Record<string, FieldError>>;
 }
 
 interface FormInputProps {
   config: FormInputConfig;
   name: string;
-  defaultValue: string;
-  errorMsg: string;
+  defaultValue: string | number;
+  errorMsg?: string;
   inputRef: (instance: HTMLInputElement | null) => void;
 }
 
 export function FormInput(props: FormInputProps) {
+  const defaultErrorMsg = (name: string) => `Field ${name} is required`;
+
   return (
     <FormGroup>
       <input
@@ -29,11 +32,18 @@ export function FormInput(props: FormInputProps) {
         placeholder={props.name}
         onChange={props.config.onChange}
         disabled={props.config.isDisabled(props.name)}
+        hidden={
+          props.config.isHidden ? props.config.isHidden(props.name) : false
+        }
         defaultValue={props.defaultValue}
         ref={props.inputRef}
       />
       <span className="text-danger">
-        {props.config.errors[props.name] ? props.errorMsg : ""}
+        {props.config.errors[props.name]
+          ? props.errorMsg
+            ? props.errorMsg
+            : defaultErrorMsg(props.name)
+          : ""}
       </span>
     </FormGroup>
   );
