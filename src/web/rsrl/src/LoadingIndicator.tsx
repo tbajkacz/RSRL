910bb4bccf508b342dad4db433f776bf784a1c8a
Tomasz Bajkacz <menidas98@gmail.com>
2020-01-07 18:50:44 +0100

    Added an option to inline and change icon size

--- src/web/rsrl/src/LoadingIndicator.tsx
+++ src/web/rsrl/src/LoadingIndicator.tsx
@@ -7,6 +7,8 @@ interface LoadingIndicatorProps {
   children?: JSX.Element;
   promise: Promise<any> | undefined;
   asModal?: boolean;
+  inline?: boolean;
+  size?: "small" | "medium" | "large";
 }
 
 export default function LoadingIndicator(props: LoadingIndicatorProps) {
@@ -16,12 +18,21 @@ export default function LoadingIndicator(props: LoadingIndicatorProps) {
     setIsCompleted(false);
   }, [props.promise]);
 
+  const getSize = () => {
+    switch (props.size) {
+      case "small":
+        return "1x";
+      case "large":
+        return "4x";
+      case "medium":
+      default:
+        return "2x";
+    }
+  };
+
   const renderLoadingIndicator = () => {
-    const loadingIndicator = (
-      <div className="d-flex justify-content-center">
-        <FontAwesomeIcon icon={faSpinner} spin={true} color="white" size="2x" />
-      </div>
-    );
+    const icon = <FontAwesomeIcon icon={faSpinner} spin={true} color="white" size={getSize()} />;
+    const loadingIndicator = props.inline ? icon : <div className="d-flex justify-content-center">{icon}</div>;
     if (props.asModal) {
       return (
         <Modal className="ui-bg-transparent" isOpen={!isCompleted}>
@@ -41,7 +52,7 @@ export default function LoadingIndicator(props: LoadingIndicatorProps) {
         setIsCompleted(true);
       }
     );
-    return <div>{isCompleted ? props.children : renderLoadingIndicator()}</div>;
+    return <>{isCompleted ? props.children : renderLoadingIndicator()}</>;
   }
   return <div>{props.children}</div>;
 }
