07b2a1d75065413e232ee8efde6fe38ae5fc82c4
Tomasz Bajkacz <menidas98@gmail.com>
2020-01-22 13:28:41 +0100

    Added appsettings.json generation in install.bat

--- src/install.bat
+++ src/install.bat
@@ -1,7 +1,15 @@
+set "SRVNAME=RSRL"
+set "SRVPROJ=RSRL.Api"
 set "WEBNAME=rsrl"
+
 set "WEBBUILD=web\%WEBNAME%\build"
 
 cd web\%WEBNAME%\
 call npm install
+
+cd ..\..
+cd srv\%SRVNAME%\%SRVPROJ%\
+
+COPY appsettings.json.sample appsettings.json
 
 cd ..\..
